77c7a2d4d6851f674d7b8561e4ff9ce32661503b
tony <tony@ak.com>
2019-05-15 20:51:44 +0800

    flutterboost

---
+++ HybridFlutter/iOS/HybridiOS/HybridiOS/FlutterSection/MyFlutterRouter.h
@@ -0,0 +1,17 @@
+//
+//  MyFlutterRouter.h
+//  HybridiOS
+//
+//  Created by 娜娜子 on 15/05/2019.
+//  Copyright © 2019 Lonely traveller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyFlutterRouter : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HybridFlutter/iOS/HybridiOS/HybridiOS/FlutterSection/MyFlutterRouter.m
@@ -0,0 +1,13 @@
+//
+//  MyFlutterRouter.m
+//  HybridiOS
+//
+//  Created by 娜娜子 on 15/05/2019.
+//  Copyright © 2019 Lonely traveller. All rights reserved.
+//
+
+#import "MyFlutterRouter.h"
+
+@implementation MyFlutterRouter
+
+@end
